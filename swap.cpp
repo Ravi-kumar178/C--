85f5ae766fb7ecab2a8783ff4f715352aab8b583
Ravi-kumar178 <bhardwajravi2025@gmail.com>
2024-03-08 10:59:52 +0530

    asawq

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+//swapping
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    int b = 20;
+    int temp;
+    
+    temp = a;
+    a = b;
+    b = temp;
+    
+    cout<<"a is: "<<a<<endl;
+    cout<<"b is: "<<b<<endl;
+}
